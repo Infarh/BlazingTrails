e755bf8091997267a749b26f092bd23649fa1252
Infarh <shmachilin@gmail.com>
2024-03-13 10:57:21 +0300

    Модели данных для определения тропы (маршрута)

---
+++ BlazingTrails.Client/Features/Home/Trail.cs
@@ -0,0 +1,22 @@
+﻿namespace BlazingTrails.Client.Features.Home;
+
+public class Trail
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+    
+    public string Description { get; set; } = null!;
+    
+    public string Image { get; set; } = null!;
+    
+    public string Location { get; set; } = null!;
+    
+    public int TimeInMinutes { get; set; }
+
+    public string TimeFormatted => $"{TimeInMinutes / 60}h {TimeInMinutes % 60}m";
+
+    public int Length { get; set; }
+
+    public IEnumerable<RouteInstruction> Route { get; set; } = [];
+}
