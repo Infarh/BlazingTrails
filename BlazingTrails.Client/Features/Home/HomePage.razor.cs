2542bd37cb2755fd18f4374f7e1a5515b72f529a
Infarh <shmachilin@gmail.com>
2024-03-19 09:45:14 +0300

    Разделение компонента главной траницы на файл с кодом и файл с разметкой

---
+++ BlazingTrails.Client/Features/Home/HomePage.razor.cs
@@ -0,0 +1,25 @@
+﻿using System.Net.Http.Json;
+
+using Microsoft.AspNetCore.Components;
+
+namespace BlazingTrails.Client.Features.Home;
+
+public partial class HomePage
+{
+    private IEnumerable<Trail>? _Trails;
+
+    [Inject]
+    public HttpClient Http { get; set; } = null!;
+
+    protected override async Task OnInitializedAsync()
+    {
+        try
+        {
+            _Trails = await Http.GetFromJsonAsync<IEnumerable<Trail>>("trails/trail-data.json");
+        }
+        catch (HttpRequestException error)
+        {
+            Console.WriteLine($"Проблема с загрузкой данных: {error.Message}");
+        }
+    }
+}
