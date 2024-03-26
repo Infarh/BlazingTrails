cde5aa0d9f9623e8f0ea477df10461ad8e0e0a4e
Павел <shmachilin@gmail.com>
2024-03-27 00:05:16 +0300

    Определение запроса и обрабобтчика запроса создания новой тропы

---
+++ BlazingTrails.Shared/Features/ManageTrails/AddTrailHandler.cs
@@ -0,0 +1,19 @@
+﻿using System.Net.Http.Json;
+using MediatR;
+
+namespace BlazingTrails.Shared.Features.ManageTrails;
+
+public class AddTrailHandler(HttpClient http) : IRequestHandler<AddTrailRequest, AddTrailRequest.Response>
+{
+    public async Task<AddTrailRequest.Response> Handle(AddTrailRequest request, CancellationToken Cancel)
+    {
+        var response = await http.PostAsJsonAsync(AddTrailRequest.RouteTemplate, request, Cancel).ConfigureAwait(false);
+
+        if (!response.IsSuccessStatusCode)
+            return new(-1);
+
+        var trail_id = await response.Content.ReadFromJsonAsync<int>(cancellationToken: Cancel).ConfigureAwait(false);
+
+        return new(trail_id);
+    }
+}
---
+++ BlazingTrails.Shared/Features/ManageTrails/AddTrailRequest.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using MediatR;
+
+namespace BlazingTrails.Shared.Features.ManageTrails;
+
+public record AddTrailRequest(TrailDTO Trail) : IRequest<AddTrailRequest.Response>
+{
+    public const string RouteTemplate = "/api/trails";
+
+    public record Response(int TrailId);
+}
+
+public class AddTrailRequestValidator : AbstractValidator<AddTrailRequest>
+{
+    public AddTrailRequestValidator() => RuleFor(r => r.Trail).SetValidator(new TrailDTOValidator());
+}
