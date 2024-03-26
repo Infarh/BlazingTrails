6914bec436a67800e0f82635d5b6b0128cb86ca3
Павел <shmachilin@gmail.com>
2024-03-26 23:19:19 +0300

    Добавление MediatR

--- BlazingTrails.Client/Program.cs
+++ BlazingTrails.Client/Program.cs
@@ -8,6 +8,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(_ => new HttpClient { BaseAddress = new(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddSassCompiler();
+builder.Services.AddMediatR(opt => opt.RegisterServicesFromAssemblyContaining<Program>());
 
 var app = builder.Build();
 
