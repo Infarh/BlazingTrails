145736313328352f9c0bf2f5cf86a102fc7818c1
Infarh <shmachilin@gmail.com>
2024-03-28 12:27:41 +0300

    Добавление запроса на загрузку файла с картинкой тропы на сервер

---
+++ BlazingTrails.Client/Features/ManageTrails/UploadTrailImageHandler.cs
@@ -0,0 +1,36 @@
+﻿using BlazingTrails.Shared.Features.ManageTrails;
+
+using MediatR;
+
+namespace BlazingTrails.Client.Features.ManageTrails;
+
+public class UploadTrailImageHandler(
+    HttpClient Http, 
+    ILogger<UploadTrailImageHandler> Logger)
+    : IRequestHandler<UploadTrailImageRequest, UploadTrailImageRequest.Response>
+{
+    public async Task<UploadTrailImageRequest.Response> Handle(UploadTrailImageRequest request, CancellationToken Cancel)
+    {
+        Logger.LogInformation("Загрузка файла с изображением тропы на сервер");
+
+        await using var file_content = request.File.OpenReadStream(request.File.Size, Cancel);
+
+        using var content = new MultipartFormDataContent();
+        content.Add(new StreamContent(file_content), "image", request.File.Name);
+
+        var response = await Http.PostAsync(UploadTrailImageRequest.RouteTemplate.Replace("{TrailId}", request.TrailId.ToString()), content, Cancel);
+
+        if (response.IsSuccessStatusCode)
+        {
+            var file_name = await response.Content.ReadAsStringAsync(cancellationToken: Cancel);
+
+            Logger.LogInformation("Загрузка файла с изображением тропы на сервер выполнено успешно");
+            return new(file_name);
+        }
+        else
+        {
+            Logger.LogWarning("Ошибка загрузки файла картинки тропы на сервер");
+            return new("");
+        }
+    }
+}
